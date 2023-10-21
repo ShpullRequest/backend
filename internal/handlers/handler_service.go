89cffb8dfd4c7e9a524b5717c94c904e7166ff12
k-orolevsk-y <i@korolevsky.me>
2023-10-21 21:41:44 +0300

    methods: change section company to companies (/company/ -> /companies/)

--- internal/handlers/handler_service.go
+++ internal/handlers/handler_service.go
@@ -21,11 +21,11 @@ func ConfigureService(apiService api.Service) {
 	apiService.GetRouter().GET("/users/:vkId", hs.GetUserByVkID)
 	apiService.GetRouter().PATCH("/users/", hs.EditUser)
 
-	apiService.GetRouter().GET("/company", hs.GetAllCompanies)
-	apiService.GetRouter().GET("/company/:companyId", hs.GetCompany)
-	apiService.GetRouter().GET("/company/my", hs.GetMyCompanies)
-	apiService.GetRouter().POST("/company", hs.NewCompany)
-	apiService.GetRouter().POST("/company/:companyId/accept", hs.AcceptCompany)
+	apiService.GetRouter().GET("/companies", hs.GetAllCompanies)
+	apiService.GetRouter().GET("/companies/:companyId", hs.GetCompany)
+	apiService.GetRouter().GET("/companies/my", hs.GetMyCompanies)
+	apiService.GetRouter().POST("/companies", hs.NewCompany)
+	apiService.GetRouter().POST("/companies/:companyId/accept", hs.AcceptCompany)
 
 	apiService.GetRouter().NoRoute(hs.NoRoute)
 	apiService.GetRouter().NoMethod(hs.NoRoute)
