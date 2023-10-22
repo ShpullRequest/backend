6ab38aee7aa4517e8f9dcb0dd851d5dacf8e287c
k-orolevsk-y <i@korolevsky.me>
2023-10-22 10:37:44 +0300

    last fix cors plz

--- internal/middlewares/cors.go
+++ internal/middlewares/cors.go
@@ -1,9 +1,17 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func (ms *middlewareService) Cors(ctx *gin.Context) {
-	ctx.Header("Access-Control-Allow-Origin", "*")
-	ctx.Header("Access-Control-Allow-Headers", "*")
-	ctx.Header("Access-Control-Allow-Methods", "*")
+	if ctx.Request.Method == http.MethodOptions {
+		ctx.Header("Access-Control-Allow-Origin", "*")
+		ctx.Header("Access-Control-Allow-Headers", "*")
+		ctx.Header("Access-Control-Allow-Methods", "*")
+
+		ctx.Status(http.StatusOK)
+		ctx.Abort()
+	}
 }
