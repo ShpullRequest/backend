09b8fd33a4563020e07166bcf33b879c9c12dafe
k-orolevsk-y <i@korolevsky.me>
2023-10-22 03:49:52 +0300

    events methods: end

--- internal/handlers/handler_service.go
+++ internal/handlers/handler_service.go
@@ -18,19 +18,24 @@ func ConfigureService(apiService api.Service) {
 	}
 
 	apiService.GetRouter().GET("/users/", hs.GetMe)
-	apiService.GetRouter().GET("/users/:vkId", hs.GetUserByVkID)
+	apiService.GetRouter().GET("/users/:vkId/", hs.GetUserByVkID)
 	apiService.GetRouter().PATCH("/users/", hs.EditUser)
 
 	apiService.GetRouter().GET("/companies/", hs.GetAllCompanies)
-	apiService.GetRouter().GET("/companies/:companyId", hs.GetCompany)
-	apiService.GetRouter().GET("/companies/my", hs.GetMyCompanies)
+	apiService.GetRouter().GET("/companies/:companyId/", hs.GetCompany)
+	apiService.GetRouter().GET("/companies/my/", hs.GetMyCompanies)
 	apiService.GetRouter().POST("/companies/", hs.NewCompany)
-	apiService.GetRouter().POST("/companies/:companyId/accept", hs.AcceptCompany)
+	apiService.GetRouter().POST("/companies/:companyId/accept/", hs.AcceptCompany)
 
 	apiService.GetRouter().GET("/events/", hs.GetAllEvents)
-	apiService.GetRouter().GET("/events/:eventId", hs.GetEvent)
+	apiService.GetRouter().GET("/events/company/:companyId", hs.GetCompanyEvents)
+	apiService.GetRouter().GET("/events/search/:query/", hs.SearchEvents)
+	apiService.GetRouter().GET("/events/:eventId/", hs.GetEvent)
+	apiService.GetRouter().GET("/events/:eventId/reviews/", hs.GetReviewsEvent)
+	apiService.GetRouter().POST("/events/:eventId/reviews/", hs.NewReviewEvent)
 	apiService.GetRouter().POST("/events/", hs.NewEvent)
-	apiService.GetRouter().PATCH("/events/:eventId", hs.SaveEvent)
+	apiService.GetRouter().PATCH("/events/:eventId/reviews/", hs.EditReviewsEvent)
+	apiService.GetRouter().PATCH("/events/:eventId/", hs.EditEvent)
 
 	apiService.GetRouter().NoRoute(hs.NoRoute)
 	apiService.GetRouter().NoMethod(hs.NoRoute)
